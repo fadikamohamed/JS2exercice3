81365e8094d8d06fdef5af61e27bde07d8776496
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-23 11:09:21 +0200

    Ajout modif addEventListener

--- assets/JS/fonction.js
+++ assets/JS/fonction.js
@@ -1,3 +1,7 @@
+
+presskey = document.getElementById('lastname');
+presskey.addEventListener('keypress',keypress);
+
 function keypress(){
   var nom = document.getElementById('lastname').value;
   alert(nom);
